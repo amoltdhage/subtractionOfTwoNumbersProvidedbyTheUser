b4757beef3f3549bbfd029686db2647945c85c11
Amol Dhage <amoltdhage@gmail.com>
2019-12-29 02:25:31 +0530

    File Added

---
+++ subtractionOfTwoNumbersProvidedByTheUser.py
@@ -0,0 +1,7 @@
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+# sub two numbers
+sum = float(num1) - float(num2)
+# Display the sub
+print('The sub of {0} and {1} is {2}'.format(num1, num2, sum))
